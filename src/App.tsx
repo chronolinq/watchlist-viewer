31ccc4daabe1c0dd0e1ae4ad9386d4b4ba015f1c
Wilson Wong <wilsonwongj@gmail.com>
2024-11-25 23:23:30 -0800

    [Changed] Basic visualization
    Show movies in a simple list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,57 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import watchlistItems from "../public/tmdb-watchlist.json" assert { type: "json" };
 import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { TmdbMovie } from "./models/tmdb";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+  const items: TmdbMovie[] = watchlistItems satisfies TmdbMovie[];
+  const listItems = items.sort(sortMovies).map(renderMovie);
 
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
-  );
+  if (!items || items.length === 0) {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div>No movies</div>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div>{listItems}</div>
+      </React.Fragment>
+    );
+  }
 }
+
+const renderMovie = (movie: TmdbMovie) => (
+  <div key={movie.id} id={movie.id.toString()}>
+    <img
+      src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
+      width="92"
+    ></img>
+    <div>
+      {movie.title} (
+      {!movie.release_date ? "TBD" : movie.release_date.substring(0, 4)})
+    </div>
+  </div>
+);
+
+const sortMovies = (aMovie: TmdbMovie, bMovie: TmdbMovie): 0 | 1 | -1 => {
+  if (!aMovie.release_date) {
+    return 1;
+  } else if (!bMovie.release_date) {
+    return -1;
+  }
+
+  if (aMovie.release_date < bMovie.release_date) {
+    return -1;
+  } else if (aMovie.release_date > bMovie.release_date) {
+    return 1;
+  }
+
+  return 0;
+};
 
 export default App;
